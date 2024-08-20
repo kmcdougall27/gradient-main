9d50144a67f7963a5a3829724be95ab338795c88
kmcdougall27 <loptwo@gmail.com>
2024-08-20 09:39:55 -0700

    Update pom.xml and GradientMainApplication.java

--- src/main/java/com/gradient/gradientmain/GradientMainApplication.java
+++ src/main/java/com/gradient/gradientmain/GradientMainApplication.java
@@ -1,8 +1,6 @@
 package com.gradient.gradientmain;
 
-import nu.pattern.OpenCV;
 import org.opencv.core.Core;
-import org.opencv.core.Mat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
